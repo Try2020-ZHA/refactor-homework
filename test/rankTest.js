186728c311a52876e586b41cf32c070db52bbaa5
Keanu.Wang <386247915@qq.com>
2020-08-27 20:30:31 +0800

    [homework][Keanu] test: add 5 test about captainHistoryRisk function

--- test/rankTest.js
+++ test/rankTest.js
@@ -75,3 +75,140 @@ rankTest('should return true if history includes china',t=>{
   const result=hasChina(history);
   t.is(result,true);
 })
+
+rankTest('calculate the captainHistoryRisk when history<5 and all profit>0 and doesn\'t contain china',t=>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: 2,
+    },{
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+  const result=captainHistoryRisk(voyage,history);
+  t.is(result,5);
+})
+
+rankTest('calculate the captainHistoryRisk when history>5 and all profit>0 and doesn\'t contain china',t=>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: 2,
+    },{
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-africa',
+      profit: 1
+    },{
+      zone: 'east-asia',
+      profit: 1
+    }
+  ];
+  const result=captainHistoryRisk(voyage,history);
+  t.is(result,1);
+})
+
+rankTest('calculate the captainHistoryRisk when history>5 and 2 profit<0 and doesn\'t contain china',t=>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: -15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },{
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-africa',
+      profit: 1
+    },{
+      zone: 'east-asia',
+      profit: 1
+    }
+  ];
+  const result=captainHistoryRisk(voyage,history);
+  t.is(result,3);
+})
+
+rankTest('calculate the captainHistoryRisk when history>5 and all profit>0 and about china',t=>{
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: 2,
+    },{
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-africa',
+      profit: 1
+    },{
+      zone: 'east-asia',
+      profit: 1
+    }
+  ];
+  const result=captainHistoryRisk(voyage,history);
+  t.is(result,0);
+})
+
+rankTest('calculate the captainHistoryRisk when history<5 and 1 profit<0 and about china',t=>{
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  const history = [{
+      zone: 'china',
+      profit: 2,
+    },{
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-africa',
+      profit: -1
+    },{
+      zone: 'east-asia',
+      profit: 1
+    }
+  ];
+  const result=captainHistoryRisk(voyage,history);
+  t.is(result,4);
+})
