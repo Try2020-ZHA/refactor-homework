448f6c4f83e237a9cfde8573070f973356636ba6
Keanu.Wang <386247915@qq.com>
2020-08-27 21:52:09 +0800

    [homework][Keanu] refactor: rename the magic variable of rating function and format the document

--- src/rank.js
+++ src/rank.js
@@ -2,14 +2,13 @@ const voyage = {
   zone: 'west-indies',
   length: 10,
 };
-const history = [
-  {
+const history = [{
     zone: 'east-indies',
     profit: 5,
-  },{
+  }, {
     zone: 'west-indies',
     profit: 15,
-  },{
+  }, {
     zone: 'china',
     profit: -2,
   },
@@ -19,62 +18,64 @@ const history = [
   },
 ];
 
-function hasChina (history) {
+function hasChina(history) {
   return history.some(v => 'china' === v.zone);
 }
 
-function isContainChinaOrEastIndies(voyage){
-  return voyage.zone==='china'||voyage.zone==='east-indies';
+function isContainChinaOrEastIndies(voyage) {
+  return voyage.zone === 'china' || voyage.zone === 'east-indies';
 }
 
-function aboutChina(voyage,history){
+function aboutChina(voyage, history) {
   return voyage.zone === 'china' && hasChina(history)
 }
 
-function voyageRisk (voyage) {
+function voyageRisk(voyage) {
   let result = 1;
-  voyage.length > 4?result += 2:result;
-  voyage.length > 8?result += voyage.length - 8:result;
-  return isContainChinaOrEastIndies(voyage)?result += 4:result;
+  voyage.length > 4 ? result += 2 : result;
+  voyage.length > 8 ? result += voyage.length - 8 : result;
+  return isContainChinaOrEastIndies(voyage) ? result += 4 : result;
 }
 
-function captainHistoryRisk (voyage, history) {
+function captainHistoryRisk(voyage, history) {
   let result = 1;
-  history.length < 5?result += 4:result;
+  history.length < 5 ? result += 4 : result;
   result += history.filter(v => v.profit < 0).length;
-  return Math.max(aboutChina(voyage,history)?result -= 2:result, 0);
+  return Math.max(aboutChina(voyage, history) ? result -= 2 : result, 0);
 }
 
-function voyageProfitFactor (voyage, history) {
+function voyageProfitFactor(voyage, history) {
   let result = 2;
-  isContainChinaOrEastIndies(voyage)?result += 1:result;
-  if (aboutChina(voyage,history)) {
+  isContainChinaOrEastIndies(voyage) ? result += 1 : result;
+  if (aboutChina(voyage, history)) {
     result += 3;
-    history.length > 10?result += 1:result;
-    voyage.length > 12?result += 1:result;
-    voyage.length > 18?result -= 1:result;
-  }
-  else {
-    history.length > 8?result += 1:result;
-    voyage.length > 14?result -= 1:result;
+    history.length > 10 ? result += 1 : result;
+    voyage.length > 12 ? result += 1 : result;
+    voyage.length > 18 ? result -= 1 : result;
+  } else {
+    history.length > 8 ? result += 1 : result;
+    voyage.length > 14 ? result -= 1 : result;
   }
   return result;
 }
 
-function rating (voyage, history) {
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > (vr + chr * 2)) {
+function rating(voyage, history) {
+  const profit = voyageProfitFactor(voyage, history);
+  const risk = voyageRisk(voyage);
+  const riskOfCaptain = captainHistoryRisk(voyage, history);
+  if (profit * 3 > (risk + riskOfCaptain * 2)) {
     return 'A';
-  }
-  else {
+  } else {
     return 'B';
   }
 }
 
 module.exports = {
-  voyageRisk,rating,voyageProfitFactor,captainHistoryRisk,hasChina
+  voyageRisk,
+  rating,
+  voyageProfitFactor,
+  captainHistoryRisk,
+  hasChina
 };
 
 const myRating = rating(voyage, history);
